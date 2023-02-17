fdc6b05182e53e70ea75f5c7dc9ba1163b1412f0
Kkrneckless <aprilhtoo1949@gmail.com>
2023-02-17 12:38:20 +0100

    Add connect_db

---
+++ connect_db.php
@@ -0,0 +1,24 @@
+<?php
+    <?php
+    Echo "Start PHP </br>";
+
+    $server = "localhost"; //servername
+    $db = 'loginsystem_db'; //databasename
+    $user = "root"; // username
+    $pass = ""; // password
+
+    $options = [
+        PDO::ATTR_ERRMODE   =>PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE   =>PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   =>false,
+    ];
+    
+    try{
+        $conn = new PDO("mysql:localhost=$server; dbname=$db", $user, $pass, $options);
+
+        echo "Connected successfully </br>";
+    }catch (PDOExpection $e){
+        echo "Connection failed: " . $e->getMessage() . "</br>" . (int)$e->getCode();
+    }
+?>
+?>
